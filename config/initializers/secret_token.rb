09a4acbb8f0f6a8eb47f4b62cfc49deca0e62ff4
Zac <mymengyu@gmail.com>
2012-11-22 23:50:05 +0800

    Initial Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buytogether::Application.config.secret_token = '721be2e28a1353aee8226f0de25d81fd4fdb5c5a889e9ad2098c8de2c904635541bce8a2c0a0a90cc3723fb1c85d58efbce874ac307c5b0cec22cc4435f2ddd8'
